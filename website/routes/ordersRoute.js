f2daa020ea1daa533eb9ecc08d4cad6cd433560b
Jaehyun Shim <jaeday@umich.edu>
2021-08-20 13:41:57 -0500

    working on orders

--- website/routes/ordersRoute.js
+++ website/routes/ordersRoute.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const stripe = require("stripe")("sk_test_51JPoR1GdJegYvYHwkG18rZQAWlMISZIl8M6UldYtgAdcqNA07szyG0UJGr7189JmOsTN0h65rGckglC1iR1WwxYw00pBwpjMWo")
 const {v4: uuidv4} = require('uuid')
-router.get("/placeorder", async(req, res) => {
+const Order = require('../models/orderModel')
+//const dest = localStorage.getItem('location')
+router.post("/placeorder", async(req, res) => {
     const{token, subtotal, currentUser, cartItems} = req.body
     try{
         const customer = await stripe.customers.create({
@@ -20,7 +22,24 @@ router.get("/placeorder", async(req, res) => {
         })
 
         if(payment){
-            res.send('Payment Done')
+            //var dest = localStorage.getItem('location')
+            const neworder = new Order({
+                name: currentUser.name,
+                email: currentUser.email,
+                userid: currentUser.userid,
+                orderItems: cartItems,
+                orderAmount: subtotal,
+                shippingAddress :{
+                    street: token.card.address_line1,
+                    city: token.card.address_city,
+                    country: token.card.address_country,
+                    zipcode: token.card.address_zip
+                },
+                transactionid: payment.source.id
+            })
+            neworder.save()
+            //console.log(dest)
+            res.send('Order placed successfully')
         }
         else{
             res.send('Payment Failed')
